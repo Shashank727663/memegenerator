f62edf0b961889d6fd215b06c145f15240fa5e51
Shashank krishu <shashankkrishu99@gmail.com>
2022-11-05 23:07:13 +0530

    open ai dalle api used for image fetching

---
+++ image fetching/image_fetch.py
@@ -0,0 +1,9 @@
+import openai
+import os
+import openai
+openai.api_key = "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"
+openai.Image.create(
+  prompt="astronaut in the ocean 3d vector art",
+  n=2,
+  size="1024x1024"
+)
